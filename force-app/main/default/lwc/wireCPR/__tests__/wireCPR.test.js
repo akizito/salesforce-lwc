c11aeb86c531f23969d1ae225e652c9e2ebe7791
akizito <akizito@realtechgrp.com>
2023-10-30 21:22:29 -0400

    Salelsforce Lightning Web Components testing

---
+++ force-app/main/default/lwc/wireCPR/__tests__/wireCPR.test.js
@@ -0,0 +1,38 @@
+import { createElement } from 'lwc';
+import WireCPR from 'c/wireCPR';
+import { CurrentPageReference } from 'lightning/navigation';
+
+//Mock realistic data
+const mockCurrentPageReference = require('./data/CurrentPageReference.json');
+
+describe('c-wire-cpr', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('renders the current page reference in <pre> tag', () => {
+        // Arrange
+        const element = createElement('c-wire-cpr', {
+            is: WireCPR
+        });
+
+        // Act
+        document.body.appendChild(element);
+
+        // select element for validation
+        const preElement = element.shadowRoot.querySelector('pre');
+        expect(preElement).not.toBeNull();
+
+        //Emit data from @wire
+        CurrentPageReference.emit(mockCurrentPageReference);
+
+        return Promise.resolve().then(() => {
+            expect(preElement.textContent).toBe(
+                JSON.stringify(mockCurrentPageReference, null, 2)
+            );
+        });
+    });
+});
